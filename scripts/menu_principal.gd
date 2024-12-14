d3fb72908d5ae667928c323e687e73111f0a9d6f
FormedNumber95 <aketza200510@gmail.com>
2024-12-14 12:23:08 +0100

    añadidas las skins
    añadido es selector de skins, pero no funciona

--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -5,6 +5,5 @@ const JUEGO="res://escenas/escenaJuego.tscn"
 func _on_btn_empezar_button_down() -> void:
 	get_tree().change_scene_to_file(JUEGO)
 
-
 func _on_btn_salir_button_down() -> void:
 	get_tree().quit()
